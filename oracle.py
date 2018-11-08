39412820be4482b7fac550fd212239ba7d73ffc5
Martin Villalba <villalba@7c0h>
2018-11-08 16:04:01 +0100

    Changed the BlogOracle format

--- oracle.py
+++ oracle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import abc
+import json
 import random
 from difflib import SequenceMatcher
 
@@ -32,7 +33,9 @@ class Oracle:
         elif rule[0] in self.blog.supported_operations():
             return self.blog.ask(rule)
         else:
-            return "I'm sorry, I didn't understand your question"
+            msg={}
+            msg['text']='I\'m sorry, I didn\'t understand your question'
+            return json.dumps(msg)
 
 
 class GeneralOracle(Oracle):
@@ -43,7 +46,21 @@ class GeneralOracle(Oracle):
         return ['general', 'professional', 'interests']
 
     def ask(self, rule):
-        return "I don't know"
+        msg = {'text': 'I don\'t know.'}
+        if rule[0] == 'general':
+            msg['text'] = random.choice(['Martin graduated from the Faculty of Mathematics, Astronomy, and Physics of the National University of Córdoba, Argentina, with a M.Sc. in computer science. He\'s currently based in Saarbrücken, Germany, working on his PhD in computational linguistics.',
+                                         'He sometimes works as his department\'s unofficial graphic designer. He designed posters, conference booklets, websites, and illustrations for multiple conferences and events.',
+                                         'His regular hobbies are drawing, music, writing, and some photography.'])
+        elif rule[0] == 'professional':
+            msg['text'] = random.choice(['He has programmed in several languages (too many), delved into Microcontrollers and Assembly, moved up to Operating Systems, and stuck the landing with some AI and Deep Learning.',
+                                         'He is one of the current system administrators for the ACL Anthology.',
+                                         'If you only have a passing interest on what he does, then he has articles explaininig his research in simple terms. <a href=\'https://7c0h.com/research.html\'>Check them out!</a>.'])
+        elif rule[0] == 'interests':
+            msg['text'] = random.choice(['His regular hobbies are drawing, music, writing, and some photography.',
+                                         'His favorite OS is Unix. More precisely, Debian. But BSD sounds tempting.',
+                                         'He prefers tabs over spaces.',
+                                         'He has dinner at 10pm.'])
+        return json.dumps(msg)
 
 
 class ProjectOracle(Oracle):
@@ -54,7 +71,8 @@ class ProjectOracle(Oracle):
         return ['proj_title', 'proj_desc', 'proj_repo']
 
     def ask(self, rule):
-        return "I don't know"
+        msg = {'text': 'I don\'t know about that yet.'}
+        return json.dumps(msg)
 
 
 class PublicationsOracle(Oracle):
@@ -65,42 +83,42 @@ class PublicationsOracle(Oracle):
         return ['last_pubs', 'pubs_between', 'pubs_with', 'pubs_venue']
 
     def ask(self, rule):
-        return "I don't know"
-
+        msg = {'text': 'I haven\'t been traind to answer that yet.'}
+        return json.dumps(msg)
 
 class BlogOracle(Oracle):
     # TODO: There should be a smarter way to query these things that having a hard-coded dictionary
-    posts_dict = {
-        'nlp_and_taylor_swift.html': 'A more polite Taylor Swift with NLP and word vectors',
-        'eyetracking_and_visual_salience.html': 'Eye-tracking and visual salience',
-        'sentiments_2_user_groups.html': 'Sentiments are the new Spam - Part 2: user groups',
-        'sentiments_are_the_new_spam.html': 'Sentiments are the new Spam - Prologue',
-        'calibrate_tablet_genius_mousepen.html': 'Genius MousePen i608 in Debian Linux',
-        'recommendation.html': 'The problem with music recommendations',
-        'voting.html': 'Voting in Argentina',
-        'i_dont_visit_that_website_anymore.html': 'I don\'t visit that website anymore',
-        'what_do_you_do_again.html': 'So, what did you say you do?',
-        'experiment_report.html': 'Experiment report',
-        'im_an_idiot.html': 'I am an idiot (a Windows Phone development story)',
-        'save_advertising.html': 'My plan to save online advertising',
-        'im_not_that_angry.html': 'I\'m not that angry',
-        'screw_your_paid_internet.html': 'Screw your ad-supported internet',
-        'neural_network.html': 'A neural network in Javascript',
-        'bye_spotify.html': 'It\'s not me, Spotify, it\'s you',
-        'guitar_music_and_i.html': 'Guitar music and I',
-        'semantic_and_observational.html': 'The Semantic and Observational models',
-        'tapiz.html': 'The Tapiz instruction-giving system',
-        'give_challenge.html': 'What is the GIVE Challenge?',
-        'training_and_testing.html': 'What are training and testing?',
-        'diy_tumbler.html': 'DIY Tumbler skins, the math way',
-        'tweet_test.html': 'I\'m also trying to have tweets here',
-        'random_links_1.html': 'Random links (I)',
-        'why_ill_never_be_rich.html': 'Why I\'ll never be rich',
-        'testing_code.html': 'Testing code',
-        'almost_there.html': 'Almost there!',
-        'setting_things_up.html': 'Setting everything up',
-        'first_post.html': 'First post!'}
-    post_last = 'nlp_and_taylor_swift.html'
+
+    posts_dict = [
+        ('nlp_and_taylor_swift.html', 'A more polite Taylor Swift with NLP and word vectors'),
+        ('eyetracking_and_visual_salience.html', 'Eye-tracking and visual salience'),
+        ('sentiments_2_user_groups.html','Sentiments are the new Spam - Part 2: user groups'),
+        ('sentiments_are_the_new_spam.html', 'Sentiments are the new Spam - Prologue'),
+        ('calibrate_tablet_genius_mousepen.html', 'Genius MousePen i608 in Debian Linux'),
+        ('recommendation.html', 'The problem with music recommendations'),
+        ('voting.html', 'Voting in Argentina'),
+        ('i_dont_visit_that_website_anymore.html', 'I don\'t visit that website anymore'),
+        ('what_do_you_do_again.html', 'So, what did you say you do?'),
+        ('experiment_report.html', 'Experiment report'),
+        ('im_an_idiot.html', 'I am an idiot (a Windows Phone development story)'),
+        ('save_advertising.html', 'My plan to save online advertising'),
+        ('im_not_that_angry.html', 'I\'m not that angry'),
+        ('screw_your_paid_internet.html', 'Screw your ad-supported internet'),
+        ('neural_network.html', 'A neural network in Javascript'),
+        ('bye_spotify.html', 'It\'s not me, Spotify, it\'s you'),
+        ('guitar_music_and_i.html', 'Guitar music and I'),
+        ('semantic_and_observational.html', 'The Semantic and Observational models'),
+        ('tapiz.html', 'The Tapiz instruction-giving system'),
+        ('give_challenge.html', 'What is the GIVE Challenge?'),
+        ('training_and_testing.html', 'What are training and testing?'),
+        ('diy_tumbler.html', 'DIY Tumbler skins, the math way'),
+        ('tweet_test.html', 'I\'m also trying to have tweets here'),
+        ('random_links_1.html', 'Random links (I)'),
+        ('why_ill_never_be_rich.html', 'Why I\'ll never be rich'),
+        ('testing_code.html', 'Testing code'),
+        ('almost_there.html', 'Almost there!'),
+        ('setting_things_up.html', 'Setting everything up'),
+        ('first_post.html', 'First post!')]
     base_url = "https://www.7c0h.com/blog/new/"
 
     def __init__(self):
@@ -110,40 +128,41 @@ class BlogOracle(Oracle):
         return ['blog_last', 'blog_titles', 'blog_post', 'blog_random']
 
     def ask(self, rule):
-        msg = "I don't know"
+        msg = {'text': 'I don\'t know'}
         if rule is None:
-            return msg
+            return json.dumps(msg)
         if rule[0] == 'blog_last':
-            msg = random.choice(['The last blog post is <a href="{}">{}</a>.',
-                                 'I think the last blog post is <a href="{}">{}</a>.',
-                                 'Here is the latest blog post: <a href="{}">{}</a>.',
-                                 'You mean the latest blog post? It is <a href="{}">{}</a>.'])
-            msg = msg.format(self.base_url + self.post_last, self.posts_dict[self.post_last])
+            msg['text'] = random.choice(['The last blog post is:',
+                                         'I think the last blog post is:',
+                                         'Here is the latest blog post:',
+                                         'You mean the latest blog post? It\'s this one:'])
+            msg['items'] = ['<a href="{}">{}</a>'.format(self.base_url + self.posts_dict[0][0], self.posts_dict[0][1])]
         elif rule[0] == 'blog_titles':
             if rule[1] > 1:
-                msg = "These are the latest {} blog posts:<br>".format(rule[1])
-                for i in range(rule[1]-1):
-                    key = list(self.posts_dict)[i]
-                    msg += "<a href='{}'>{}</a>, ".format(self.base_url + "/" + key, self.posts_dict[key])
-                key = list(self.posts_dict)[rule[1]-1]
-                msg += "and <a href='{}'>{}</a>".format(self.base_url + "/" + key, self.posts_dict[key])
+                all_msgs = []
+                msg['text'] = 'These are the latest {} blog posts:'.format(rule[1])
+                for i in range(rule[1]):
+                    key = self.posts_dict[i][0]
+                    all_msgs.append("<a href='{}'>{}</a>".format(self.base_url + key, self.posts_dict[i][1]))
+                msg['items'] = all_msgs
             else:
-                msg = self.ask(['blog_last'])
+                return self.ask(['blog_last'])
         elif rule[0] == 'blog_random':
-            msg = random.choice(['A random blog post is <a href="{}">{}</a>.',
-                                 'I think a good blog post is <a href="{}">{}</a>.',
-                                 'Here is a completely random blog post: <a href="{}">{}</a>.',
-                                 'You mean any blog post? Here\'s one: <a href="{}">{}</a>.'])
-            key = list(self.posts_dict)[0]
-            msg = msg.format(self.base_url + key, self.posts_dict[key])
+            msg['text'] = random.choice(['A random blog post is:',
+                                         'I think a good blog post is:',
+                                         'Here is a completely random blog post:',
+                                         'You mean any blog post? Here\'s one:'])
+            post = random.choice(self.posts_dict)
+            msg['items'] = ['<a href="{}">{}</a>'.format(self.base_url + post[0], post[1])]
         elif rule[0] == 'blog_post':
             ratio = 0
-            best = self.post_last
-            for key in self.posts_dict:
-                new_ratio = SequenceMatcher(None, rule[1], self.posts_dict[key]).ratio()
+            best = 0
+            for key in range(len(self.posts_dict)):
+                new_ratio = SequenceMatcher(None, rule[1], self.posts_dict[key][1]).ratio()
                 if new_ratio > ratio:
                     best = key
                     ratio = new_ratio
-            msg = 'The best match I found is this post: <a href="{}">{}</a>.'.format(self.base_url + best,
-                                                                                     self.posts_dict[best])
-        return msg
+            msg['text'] = 'The best match I found is this post:'
+            msg['item'] = ['<a href="{}">{}</a>.'.format(self.base_url + self.posts_dict[best][0],
+                                                         self.posts_dict[best][1])]
+        return json.dumps(msg)
